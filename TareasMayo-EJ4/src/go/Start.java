4f02baebc1bb65edf4133656bb83751b34622bf0
mario <mario@Mario_Computer>
2023-05-21 15:02:10 +0200

    Ejercicio Terminado

--- TareasMayo-EJ4/src/go/Start.java
+++ TareasMayo-EJ4/src/go/Start.java
@@ -9,13 +9,15 @@ import utilidades.Write;
 
 public class Start {
 
-	public static void main(String[] args) throws IOException {
-		
+	public static void main(String[] args){
+			try {
 			Concesionario primero = new Concesionario();
 			AsignarCoches asignador = new AsignarCoches();
 			Write escritor = new Write();
 			escritor.escribir(primero, asignador, "C:\\Users\\mario\\OneDrive\\Escritorio\\Archivos de texto Eclipse\\EJ4_MAYO\\RESULTADO.txt");
-			
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
 			
 		
 		
